ef48112f36aabb95d876eab5c73578b45f91052e
Flavio Garcia <piraz@candango.org>
2008-02-27 01:05:32 +0000

    Fixing links

--- myfuses_tools/tools/dspHeader.php
+++ myfuses_tools/tools/dspHeader.php
@@ -1,6 +1,6 @@
 <h1>MyFuses Tools</h1>
 <div style="color:red;"><?=MyFusesApplicationSecurityPlugin::getMessage()?></div>
 <?if( MyFusesApplicationSecurityPlugin::isLogged() ){?>
-<a href="<?=MyFuses::getMySelfXfa( "goToStart" )?>">Index</a>
-<a href="<?=MyFuses::getMySelfXfa( "goToLogout" )?>">Logout</a>
+Main: <a href="<?=MyFuses::getMySelfXfa( "goToStart" )?>">Index</a>
+<a href="<?=MyFuses::getMySelfXfa( "goToLogout" )?>">Logout</a><br>
 <?}?>
