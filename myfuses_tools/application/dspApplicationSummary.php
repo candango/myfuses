ed79e31a12c7439f8988d013230350a4452d452b
Flavio Garcia <piraz@candango.org>
2008-02-27 00:41:44 +0000

    Adding application parameters

--- myfuses_tools/application/dspApplicationSummary.php
+++ myfuses_tools/application/dspApplicationSummary.php
@@ -1,6 +1,6 @@
-<h3>Application Summary</h3>
+<h2>Application Summary</h2>
 
-<h4>Circuits</h4>
+<h3>Circuits</h3>
 
 <table border="1">
   <tr>
@@ -15,8 +15,18 @@
         <td><?=$circuit->getName()?></td>
         <td><?=$circuit->getPath()?></td>
         <td><?=$circuit->getFile()?></td>
-        <td><?=$circuit->getAccess()?></td>
+        <td><?=$circuit->getAccessName()?></td>
     </tr>
     <?}?>
 <?}?>
 </table>
+
+<h3>Parameters</h3>
+
+<b>Fuseaction Variable</b>: <?=$application->getFuseactionVariable()?><br/>
+<b>Default Fusesaction</b>: <?=$application->getDefaultFuseaction()?><br/>
+<b>Mode</b>: <?=$application->getMode()?><br/>
+<b>Parse With Enabled</b>: <?=$application->isParsedWithComments() ? "yes" : "no"?><br/>
+<b>Character Encoding</b>: <?=$application->getCharacterEncoding()?><br/>
+<b>Debug Enabled</b>: <?=$application->isDebugAllowed() ? "yes" : "no"?><br/>
+<b>MyFuses Tools Enabled</b>: <?=$application->isToolsAllowed() ? "yes" : "no"?><br/>
