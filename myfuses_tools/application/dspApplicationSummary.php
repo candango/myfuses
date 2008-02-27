dd113385c99a092cc5e8b0b55930c0e14c7a2411
Flavio Garcia <piraz@candango.org>
2008-02-27 00:49:50 +0000

    Adding classes

--- myfuses_tools/application/dspApplicationSummary.php
+++ myfuses_tools/application/dspApplicationSummary.php
@@ -9,15 +9,42 @@
     <th>File</th>
     <th>Access</th>
   </tr>
-<?foreach( $application->getCircits() as $circuit ){?>
-    <?if( $circuit->getName() != 'MYFUSES_GLOBAL_CIRCUIT' ) {?>
+<?if( count( $application->getCircits() ) ) {?>  
+    <?foreach( $application->getCircits() as $circuit ) {?>
+        <?if( $circuit->getName() != 'MYFUSES_GLOBAL_CIRCUIT' ) {?>
+        <tr>
+            <td><?=$circuit->getName()?></td>
+            <td><?=$circuit->getPath()?></td>
+            <td><?=$circuit->getFile()?></td>
+            <td><?=$circuit->getAccessName()?></td>
+        </tr>
+        <?}?>
+    <?}?>
+<?}else {?>
+<tr>
+    <td colspan="4">No Circuit Founded</td>
+</tr>
+<?}?>
+</table>
+
+<h3>Classes</h3>
+
+<table border="1">
+  <tr>
+    <th>Name</th>
+    <th>Path</th>
+  </tr>
+<?if( count( $application->getClasses() ) ) {?>  
+    <?foreach( $application->getClasses() as $class ) {?>
     <tr>
-        <td><?=$circuit->getName()?></td>
-        <td><?=$circuit->getPath()?></td>
-        <td><?=$circuit->getFile()?></td>
-        <td><?=$circuit->getAccessName()?></td>
+        <td><?=$class->getName()?></td>
+        <td><?=$class->getPath()?></td>
     </tr>
     <?}?>
+<?}else {?>
+<tr>
+    <td colspan="4">No Class Founded</td>
+</tr>
 <?}?>
 </table>
 
