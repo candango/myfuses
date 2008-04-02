8f918b40fa2e4635eb5349c7aca99f8461fa7c4b
Flavio Garcia <piraz@candango.org>
2008-04-02 18:07:01 +0000

    Fixing form Changing text field to password field

--- myfuses_tools/tools/dspFormLogin.php
+++ myfuses_tools/tools/dspFormLogin.php
@@ -2,6 +2,6 @@ You're not logged in MyFuses Tools application. Please inform your
 <?=MyFuses::getApplication()->getName()?> application password:<br>
 <form name="frmMyfusesLogin" method="post" 
     action="<?=MyFuses::getMySelfXfa( "goToLogin" )?>" >
-    <input type="text" name="myfusesLogin">
+    <input type="password" name="myfusesLogin">
     <input type="submit">
 </form>
