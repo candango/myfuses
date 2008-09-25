1631526670029bf26b990bcb3c327337e1d88c51
Flavio Garcia <piraz@candango.org>
2008-09-25 19:46:13 +0000

    Sending to login action if credential was not authenticated. Refers #14.

--- plugins/MyFusesBasicSecurityPlugin.class.php
+++ plugins/MyFusesBasicSecurityPlugin.class.php
@@ -12,6 +12,25 @@ class MyFusesBasicSecurityPlugin extends MyFusesAbstractSecurityPlugin{
 
         public function configureSecurityManager( MyFusesSecurityManager $manager ) {
             
+            // getting login action
+            $loginAction = $this->getParameter( 'LoginAction' );
+            
+            $loginAction = $loginAction[ 0 ];
+            
+            $currentAction = MyFuses::getInstance()->getRequest()->
+                getFuseActionName();
+            
+            MyFuses::getInstance()->getRequest()->getAction()->addXFA( 
+                'goToLoginPage', $loginAction );
+            
+            
+            if( $loginAction != $currentAction ) {
+                if( !$manager->isAuthenticated() ) {
+                    MyFuses::sendToUrl( MyFuses::getMySelfXfa( 
+                        'goToLoginPage' ) );
+                }
+            }
+            
         }
         
 }
