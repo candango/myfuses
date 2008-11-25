a6380df16497f191c2e13923ff4040f397a60a3a
Flavio Garcia <piraz@candango.org>
2008-11-25 18:23:39 +0000

    Adding header comment.

--- plugins/MyFusesAbstractSecurityPlugin.class.php
+++ plugins/MyFusesAbstractSecurityPlugin.class.php
@@ -1,6 +1,62 @@
 <?php
+/**
+ * MyFusesAbstractSecurityPlugin  - MyFusesAbstractSecurityPlugin.class.php
+ * 
+ * Plugin that controls all authentication and authorization flow 
+ * over an application.
+ * 
+ * PHP version 5
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is Fuses "a Candango implementation of Fusebox Corporation 
+ * Fusebox" part .
+ * 
+ * The Initial Developer of the Original Code is Flávio Gonçalves Garcia.
+ * Portions created by Flávio Gonçalves Garcia are Copyright (C) 2006 - 2007.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flávio Gonçalves Garcia.
+ *
+ * @category   plugins
+ * @package    myfuses.plugins
+ * @author     Flavio Gonçalves Garcia <flavio.garcia@candango.org>
+ * @copyright  Copyright (c) 2006 - 2008 Candango Opensource Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id: IParseable.class.php 205 2007-12-18 12:47:40Z flavio.garcia $
+ */
+
 require_once 'myfuses/util/security/MyFusesAbstractSecurityManager.class.php';
 
+/**
+ * MyFusesAbstractSecurityPlugin  - MyFusesAbstractSecurityPlugin.class.php
+ * 
+ * Plugin that controls all authentication and authorization flow 
+ * over an application.
+ * 
+ * PHP version 5
+ *
+ * @category   plugins
+ * @package    myfuses.plugins
+ * @author     Flavio Gonçalves Garcia <flavio.garcia@candango.org>
+ * @copyright  Copyright (c) 2006 - 2008 Candango Opensource Group
+ * @link http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Revision: 205 $
+ * @since      Revision 25
+ */
 abstract class MyFusesAbstractSecurityPlugin extends AbstractPlugin {
     
     /**
