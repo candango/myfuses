4129fce7896b825f2797c1b811051d8a2c418cf8
Flavio Garcia <piraz@candango.org>
2009-03-19 19:10:52 +0000

    Adding reader coments and MYFUSES_ROOT_PATH controller constant.

--- noxml/myfuses/MyFuses.class.php
+++ noxml/myfuses/MyFuses.class.php
@@ -1,4 +1,43 @@
 <?php
+/**
+ * MyFuses - MyFuses.class.php
+ * 
+ * This is MyFuses a Candango Opensource Group a implementation of Fusebox 
+ * Corporation Fusebox framework. The MyFuses is used as Iflux Framework 
+ * Main Controller.
+ * 
+ * PHP version 5
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is myFuses "a Candango implementation of Fusebox Corporation 
+ * Fusebox" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2006 - 2009.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ *
+ * @category   controller
+ * @package    myfuses
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2006 - 2009 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id: MyFuses.class.php 662 2009-03-11 04:30:31Z flavio.garcia $
+ */
 define( "MYFUSES_ROOT_PATH", dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 
 require_once "myfuses/core/Application.class.php";
@@ -7,11 +46,32 @@ require_once "myfuses/core/Circuit.class.php";
 require_once "myfuses/engine/MyFusesLoader.class.php";
 
 require_once "myfuses/process/MyFusesLifecycle.class.php";
+require_once "myfuses/process/MyFusesRequest.class.php";
 
 require_once "myfuses/util/Common.classes.php";
 
+/**
+ * MyFuses - MyFuses.class.php
+ * 
+ * This is MyFuses a Candango Opensource Group a implementation of Fusebox 
+ * Corporation Fusebox framework. The MyFuses is used as Iflux Framework 
+ * Main Controller.
+ * 
+ * PHP version 5
+ *
+ * @category   controller
+ * @package    myfuses
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2006 - 2009 Candango Open Source Group
+ * @link http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Revision: 662 $
+ * @since      Revision 17
+ */
 class MyFuses {
     
+	const MYFUSES_ROOT_PATH = MYFUSES_ROOT_PATH;
+	
     /**
      * Array of registered applications
      * 
