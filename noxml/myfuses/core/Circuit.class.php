7e37f9f18ad47451094133aa1d3c1c98f3a89a7d
Flavio Garcia <piraz@candango.org>
2009-03-19 13:17:52 +0000

    Introducing CircuitReference. Refers #37.

--- noxml/myfuses/core/Circuit.class.php
+++ noxml/myfuses/core/Circuit.class.php
@@ -48,3 +48,67 @@ abstract class AbstractCircuit implements Circuit {
 class BasicCircuit extends AbstractCircuit {
 	
 }
+
+interface CircuitReference {
+	
+	public function getName();
+    
+    public function setName( $name ); 
+    
+    public function getPath();
+    
+    public function setPath( $path );
+    
+    public function getParent();
+    
+    public function setParent( $parent );
+    
+}
+
+abstract class AbstractCircuitReference implements CircuitReference {
+    
+    /**
+     * Application name
+     * 
+     * @access private
+     */
+    private $name;
+    
+    /**
+     * Application path
+     * 
+     * @access private
+     */
+    private $path;
+    
+    private $parent;
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function setName( $name ) {
+        $this->name = $name;
+    }
+    
+    public function getPath() {
+        return $this->path;
+    }
+    
+    public function setPath( $path ) {
+        $this->path = $path;
+    }
+    
+    public function getParent() {
+    	return $this->parent;
+    }
+    
+    public function setParent( $parent ) {
+    	$this->parent = $parent;
+    }
+    
+}
+
+class BasicCircuitReference extends AbstractCircuitReference {
+    
+}
