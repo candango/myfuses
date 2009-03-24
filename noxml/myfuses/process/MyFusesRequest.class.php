83f69408066c3dacc2de5e451aa3715e4f193426
Flavio Garcia <piraz@candango.org>
2009-03-24 20:06:35 +0000

    Geting the current fuseaction from get or post. Refers #37.

--- noxml/myfuses/process/MyFusesRequest.class.php
+++ noxml/myfuses/process/MyFusesRequest.class.php
@@ -7,6 +7,15 @@ class MyFusesRequest {
         
     private $fuseactionVariable;
 	
+    private $currentFuseaction;
+    
+    /**
+     * Url path extra parameters
+     *
+     * @var array
+     */
+    private $extraParams = array();
+    
 	public function getApplication() {
 		return $this->application;
 	}
@@ -51,8 +60,56 @@ class MyFusesRequest {
         $this->fuseactionVariable = $fuseactionVariable;
     }
 	
+    /**
+     * Getter function for DefaultFuseaction
+     *
+     * @return string
+     */
+    public function getCurrentFuseaction(){
+        return $this->currentFuseaction;
+    }
+    
+    /**
+     * Setter function for DefaultFuseaction
+     *
+     * @param $defaultFuseaction string
+     */
+    public function setCurrentFuseaction( $currentFuseaction ){
+        $this->currentFuseaction = $currentFuseaction;
+    }
+    
 }
 
 interface MyFusesRouter {
 	
+	public function resolveRequest( MyFusesRequest $request );
+	
+}
+
+
+abstract class MyFusesAbstractRouter implements MyFusesRouter {
+	
+	public function resolveRequest( MyFusesRequest $request ) {
+		
+		$fuseactionVariable = $request->getFuseactionVariable();
+		
+        if ( isset( $_GET[ $fuseactionVariable ] ) && 
+            $_GET[ $fuseactionVariable ] != '' ) {
+            $request->setCurrentFuseaction( $_GET[ $fuseactionVariable ] );
+        }
+            
+        if ( isset( $_POST[ $fuseactionVariable ] ) && 
+            $_POST[ $fuseactionVariable ] != '' ) {
+            $request->setCurrentFuseaction( $_POST[ $fuseactionVariable ] );
+        }
+		
+        if( $request->getCurrentFuseaction() === null ) {
+        	$request->setCurrentFuseaction( $request->getDefaultFuseaction() );
+        }
+	}
+	
+}
+
+class MyFusesBasicRouter extends MyFusesAbstractRouter {
+    	
 }
