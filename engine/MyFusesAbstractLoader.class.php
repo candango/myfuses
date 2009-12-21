3ea1ae7b4ea24543999f9e65ca85e392154b17ce
Flavio Garcia <piraz@candango.org>
2009-12-21 02:27:30 +0000

    Adding header comments. Refers #57.

--- engine/MyFusesAbstractLoader.class.php
+++ engine/MyFusesAbstractLoader.class.php
@@ -1,4 +1,64 @@
 <?php
+/**
+ * MyFusesAbstractLoader - MyFusesAbstractLoader.class.php
+ * 
+ * This is an abstract implementation of MyFusesLoader interface. This class
+ * implements all required methods required by his interface and need to be
+ * extended by a concrete class to enable his instantiating. Extend this class
+ * insted implement MyFusesLoader inteface and you will save you a lot of work.
+ * 
+ * PHP version 5
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is myFuses "a Candango implementation of Fusebox Corporation 
+ * Fusebox" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2006 - 2010.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ *
+ * @category   loader
+ * @package    myfuses.loader
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2006 - 2010 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id$
+ */
+
+/**
+ * MyFusesAbstractLoader - MyFusesAbstractLoader.class.php
+ * 
+ * This is an abstract implementation of MyFusesLoader interface. This class
+ * implements all required methods required by his interface and need to be
+ * extended by a concrete class to enable his instantiating. Extend this class
+ * insted implement MyFusesLoader inteface and you will save you a lot of work.
+ * 
+ * PHP version 5
+ *
+ * @category   loader
+ * @package    myfuses.loader
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2006 - 2010 Candango Open Source Group
+ * @link http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Revision$
+ * @since      Revision 758
+ */
 abstract class MyFusesAbstractLoader implements MyFusesLoader {
 	
     
