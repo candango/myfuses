167050d181564ba9df2c5ab8f3f5a7b773354310
Flavio Garcia <piraz@candango.org>
2009-12-29 00:27:10 +0000

    Removing phase constants. Refers #54

--- core/Plugin.class.php
+++ core/Plugin.class.php
@@ -75,54 +75,6 @@ require_once MYFUSES_ROOT_PATH . "core/ProcessPlugin.class.php";
  */
 interface Plugin { // extends IParseable, ICacheable {
     
-	/**
-     * Fuseaction exception fase constant<br>
-     * Value "fuseactionException"
-     * 
-     * @var string
-     */
-    const FUSEACTION_EXCEPTION_PHASE = "fuseactionException";
-	
-    /**
-     * Pre process fase constant<br>
-     * Value "preProcess"
-     * 
-     * @var string
-     */
-    const PRE_PROCESS_PHASE = "preProcess";
-    
-    /**
-     * Pre fuseaction fase constant<br>
-     * Value "preFuseaction"
-     * 
-     * @var string
-     */
-    const PRE_FUSEACTION_PHASE = "preFuseaction";
-    
-    /**
-     * Post fuseaction fase constant<br>
-     * Value "postFuseaction"
-     * 
-     * @var string
-     */
-    const POST_FUSEACTION_PHASE = "postFuseaction";
-    
-    /**
-     * Post process fase constant<br>
-     * Value "postProcess"
-     * 
-     * @var string
-     */
-    const POST_PROCESS_PHASE = "postProcess";
-    
-    /**
-     * Process error fase constant<br>
-     * Value "processError"
-     * 
-     * @var string
-     */
-    const PROCESS_ERROR_PHASE = "processError";
-    
     /**
      * Return the plugin name
      *
