4fffe281057e702ad4a6968fc433900780ac21e3
Flavio Garcia <piraz@candango.org>
2017-10-15 17:00:43 -0400

    Changed default comment header and enforced prs-2.
    Also fixed issues on some phpdocs parts.
    
    Refs: #9 #18

--- core/verbs/LoopVerb.class.php
+++ core/verbs/LoopVerb.class.php
@@ -1,277 +1,246 @@
 <?php
 /**
- * LoopVerb  - LoopVerb.class.php
- * 
- * This verbs executes one queue of verbs as many times as they are defined.
- * 
- * PHP version 5
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- * 
- * This product includes software developed by the Fusebox Corporation 
- * (http://www.fusebox.org/).
- * 
- * The Original Code is Fuses "a Candango implementation of Fusebox Corporation 
- * Fusebox" part .
- * 
- * The Initial Developer of the Original Code is Flávio Gonçalves Garcia.
- * Portions created by Flávio Gonçalves Garcia are Copyright (C) 2006 - 2007.
- * All Rights Reserved.
- * 
- * Contributor(s): Flávio Gonçalves Garcia.
+ * MyFuses Framework (http://myfuses.candango.org)
  *
- * @category   controller
- * @package    myfuses.core.verbs
- * @author     Flavio Gonçalves Garcia <flavio.garcia@candango.org>
- * @copyright  Copyright (c) 2006 - 2007 Candango Opensource Group
- * @link       http://www.candango.org/myfuses
- * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
- * @version    SVN: $Id$
+ * This product includes software developed by the Fusebox Corporation
+ * (http://www.fusebox.org/).
+ *
+ * @link      http://github.com/candango/myfuses
+ * @copyright Copyright (c) 2006 - 2017 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
  */
 
 /**
- * LoopVerb  - LoopVerb.class.php
+ * LoopVerb  - LoopVerb.php
  * 
  * This verbs executes one queue of verbs as many times as they are defined.
- * 
- * PHP version 5
  *
  * @category   controller
  * @package    myfuses.core.verbs
- * @author     Flavio Gonçalves Garcia <flavio.garcia@candango.org>
- * @copyright  Copyright (c) 2006 - 2007 Candango Opensource Group
- * @link http://www.candango.org/myfuses
- * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
- * @version    SVN: $Revision$
- * @since      Revision 125
+ * @author     Flavio Garcia <piraz at candango.org>
+ * @since      5a0c505168c823101071032bec66f107a870dfff
  */
-class LoopVerb extends AbstractVerb {
-    
+class LoopVerb extends AbstractVerb
+{
     private $condition;
-    
+
     private $loopVerbs = array();
-    
+
     private $from;
-    
+
     private $to;
-    
+
     private $index;
-    
+
     private $step;
-    
+
     private $collection;
-    
+
     private $item;
-    
-    public function getCondition() {
+
+    public function getCondition()
+    {
         return $this->condition;
     }
-    
-    public function setCondition( $condition ) {
+
+    public function setCondition($condition)
+    {
         $this->condition = $condition;
     }
-    
-    public function getFrom() {
+
+    public function getFrom()
+    {
         return $this->from;
     }
-    
-    public function setFrom( $from ) {
+
+    public function setFrom($from)
+    {
         $this->from = $from;
     }
-    
-    public function getTo() {
+
+    public function getTo()
+    {
         return $this->to;
     }
-    
-    public function setTo( $to ) {
+
+    public function setTo($to)
+    {
         $this->to = $to;
     }
-    
-    public function getIndex() {
+
+    public function getIndex()
+    {
         return $this->index;
     }
 
-    public function setIndex( $index ) {
+    public function setIndex($index)
+    {
         $this->index = $index;
     }
-    
-    public function getStep() {
+
+    public function getStep()
+    {
         return $this->step;
     }
 
-    public function setStep( $step ) {
+    public function setStep($step)
+    {
         $this->step = $step;
     }
-    
-    public function getCollection() {
+
+    public function getCollection()
+    {
         return $this->collection;
     }
 
-    public function setCollection( $collection ) {
+    public function setCollection($collection)
+    {
         $this->collection = $collection;
     }
-    
-    public function getItem() {
+
+    public function getItem()
+    {
         return $this->item;
     }
 
-    public function setItem( $item ) {
+    public function setItem($item)
+    {
         $this->item = $item;
     }
-    
-    public function getData() {
+
+    public function getData()
+    {
         $data = parent::getData();
-        
-        if( !is_null( $this->getCondition() ) ) {
-            $data[ "condition" ] = $this->getCondition();
-        }
-        elseif( !is_null( $this->getFrom() ) ) {
-            $data[ "attributes" ][ "from" ] = $this->getFrom();
-            $data[ "attributes" ][ "to" ] = $this->getTo();
-            $data[ "attributes" ][ "index" ] = $this->getIndex();
-            if( !is_null( $this->getStep() ) ) {
-                $data[ "attributes" ][ "step" ] = $this->getStep();
+
+        if (!is_null($this->getCondition())) {
+            $data['condition'] = $this->getCondition();
+        } elseif (!is_null($this->getFrom())) {
+            $data['attributes']['from'] = $this->getFrom();
+            $data['attributes']['to'] = $this->getTo();
+            $data['attributes']['index'] = $this->getIndex();
+            if (!is_null($this->getStep())) {
+                $data['attributes']['step'] = $this->getStep();
             }
-        }
-        elseif( !is_null( $this->getCollection() ) ) {
-            $data[ "attributes" ][ "collection" ] = $this->getCollection();
-            $data[ "attributes" ][ "item" ] = $this->getItem();
-            if( !is_null( $this->getIndex() ) ) {
-                $data[ "attributes" ][ "index" ] = $this->getIndex();
+        } elseif (!is_null($this->getCollection())) {
+            $data['attributes']['collection'] = $this->getCollection();
+            $data['attributes']['item'] = $this->getItem();
+            if (!is_null($this->getIndex())) {
+                $data['attributes']['index'] = $this->getIndex();
             }
         }
 
-        if( count( $this->loopVerbs ) ) {
-            foreach( $this->loopVerbs as $verb ) {
-                $data[ "children" ][] = $verb->getData();
+        if (count($this->loopVerbs)) {
+            foreach ($this->loopVerbs as $verb) {
+                $data['children'][] = $verb->getData();
             }
         }
-        
+
         return $data;
     }
     
-    public function setData( $data ) {
-        parent::setData( $data );
-        
-        if( isset( $data[ "attributes" ][ "condition" ] ) ) {
-            $this->setCondition( $data[ "attributes" ][ "condition" ] );
-        }
-        elseif( isset( $data[ "attributes" ][ "from" ] ) ) {
+    public function setData($data)
+    {
+        parent::setData($data);
+
+        if (isset($data['attributes']['condition'])) {
+            $this->setCondition($data['attributes']['condition']);
+        } elseif (isset($data['attributes']['from'])) {
             // TODO handle mising parameter error
-            $this->setFrom( $data[ "attributes" ][ "from" ] );
-            $this->setTo( $data[ "attributes" ][ "to" ] );
-            $this->setIndex( $data[ "attributes" ][ "index" ] );
-            if( isset( $data[ "attributes" ][ "step" ] ) ) {
-                $this->setStep( $data[ "attributes" ][ "step" ] );
+            $this->setFrom($data['attributes']['from']);
+            $this->setTo($data['attributes']['to']);
+            $this->setIndex($data['attributes']['index']);
+            if (isset($data['attributes']['step'])) {
+                $this->setStep($data['attributes']['step']);
+            }
+        } elseif (isset($data['attributes']['collection'])) {
+            $this->setCollection($data['attributes']['collection']);
+            $this->setItem($data['attributes']['item']);
+            if (isset($data['attributes']['index'])) {
+                $this->setIndex($data['attributes']['index']);
             }
         }
-        elseif( isset( $data[ "attributes" ][ "collection" ] ) ) {
-            $this->setCollection( $data[ "attributes" ][ "collection" ] );
-            $this->setItem( $data[ "attributes" ][ "item" ] );
-            if( isset( $data[ "attributes" ][ "index" ] ) ) {
-                $this->setIndex( $data[ "attributes" ][ "index" ] );
-            }
+
+        if (count($data['children'])) {
+            $this->setLoopVerbs($data['children']);
         }
-        
-        if( count( $data[ "children" ] ) ) {
-            $this->setLoopVerbs( $data[ 'children' ] );
-        }
-        
     }
-    
-    private function setLoopVerbs( $children ) {
-        
-        foreach( $children as $child ){
-            $verb = AbstractVerb::getInstance( 
-                $child, $this->getAction() );
-            if( !is_null( $verb ) ) {
-                $this->addLoopVerb( $verb );
+
+    private function setLoopVerbs($children)
+    {
+        foreach ($children as $child) {
+            $verb = AbstractVerb::getInstance($child, $this->getAction());
+            if (!is_null($verb)) {
+                $this->addLoopVerb($verb);
             }
         }
-        
     }
-    
+
 	/**
      * Add one verb into loop verbs
      *
      * @param Verb $verb
      */
-    public function addLoopVerb( Verb $verb ) {
+    public function addLoopVerb( Verb $verb )
+    {
        $this->loopVerbs[] = $verb;
-       $verb->setParent( $this ); 
+       $verb->setParent($this);
     }
-    
 
     /**
      * Return the parsed code
      *
      * @return string
      */
-    public function getParsedCode( $commented, $identLevel ) {
-        $strOut = parent::getParsedCode( $commented, $identLevel );
-        $strOut .= str_repeat( "\t", $identLevel );
-        if( !is_null( $this->getCondition() ) ) {
+    public function getParsedCode($commented, $identLevel)
+    {
+        $strOut = parent::getParsedCode($commented, $identLevel);
+        $strOut .= str_repeat("\t", $identLevel);
+        if (!is_null($this->getCondition())) {
             $strOut .= "while( " . $this->getCondition() . " ) {\n";
-        }
-        elseif( !is_null( $this->getFrom() ) ) {
+        } elseif (!is_null($this->getFrom())) {
             $from = $this->getFrom();
             $to = $this->getTo();
             $index = $this->getIndex();
             $indexName = $this->getIndex();
-            if(MyFuses::strStartsWith($this->getIndex(), "$")) {
-                $indexName = str_replace("\$", "",
-                    $this->getIndex());
-            }
-            else {
+            if (MyFuses::strStartsWith($this->getIndex(), "$")) {
+                $indexName = str_replace("\$", "", $this->getIndex());
+            } else {
                 $index = "\$" . $this->getIndex();
             }
+            $step = (is_null($this->step)) ? 1 : $this->step;
 
-            $step = ( is_null( $this->step ) ) ? 1 : $this->step;
-            
             $signal = "<=";
             $signal1 = "+";
-            
-            
-            if( $from > $to ) {
+
+            if ($from > $to) {
                 $signal = "<=";
                 $signal1 = "-";
             }
-            
-            $strOut .= "for( " . $index . " = " . $from . "; " . 
-                $index . " " . $signal . " " . $to . "; " . 
-                $index . " = " . $index . " " . $signal1 . " " . $step . " ) {\n";
-            $strOut .= str_repeat( "\t", $identLevel + 1 );
-            $strOut .= $this->getVariableSetString($indexName, "#" . $index . "#");
-        }
-        elseif( !is_null( $this->getCollection() ) ) {
+
+            $strOut .= "for( " . $index . " = " . $from . "; " . $index . " " .
+                $signal . " " . $to . "; " . $index . " = " . $index . " " .
+                $signal1 . " " . $step . " ) {\n";
+            $strOut .= str_repeat("\t", $identLevel + 1);
+            $strOut .= $this->getVariableSetString(
+                $indexName, "#" . $index . "#");
+        } elseif (!is_null( $this->getCollection())) {
             $collection = $this->getCollection();
             $item = $this->getItem();
-            
+
             $strIndex = "";
-            
-            if( !is_null( $this->getIndex() ) ) {
+
+            if (!is_null($this->getIndex())) {
                 $strIndex = $this->getIndex() . " => " ;
             }
             $strOut .= "foreach( " . $collection . " as " . $strIndex . 
                 $item . " ) {\n";
         }
 
-        foreach(  $this->loopVerbs as $verb ) {
-            $strOut .= $verb->getParsedCode( $commented, $identLevel + 1 );
+        foreach ($this->loopVerbs as $verb) {
+            $strOut .= $verb->getParsedCode($commented, $identLevel + 1);
         }
-        
-        $strOut .= str_repeat( "\t", $identLevel );
-        
+        $strOut .= str_repeat("\t", $identLevel);
         $strOut .= "}\n";
         return $strOut;
     }
-
 }
--- core/verbs/debug/DebugDumpVerb.class.php
+++ core/verbs/debug/DebugDumpVerb.class.php
@@ -13,84 +13,82 @@
 /**
  * DebugDumpVerb  - DebugDumpVerb.php
  *
- * This verb delegates execution to another verb. In fact DoVerb will resolve
- * the verb called until find some "real" verb.
- *
- * PHP version 5
+ * Provides a var_dump verb. Can be helpful for debugging.
  *
  * @category   controller
  * @package    myfuses.core.verbs.debug
  * @author     Flavio Garcia <piraz at candango.org>
  * @since      f7e2c2ed2897bb16a0eab9a5c4cfc6a1fbd87205
  */
-class DebugDumpVerb extends AbstractVerb {
-    
+class DebugDumpVerb extends AbstractVerb
+{
     private $variable;
-    
+
     private $die = false;
-    
-    public function getVariable(){
+
+    public function getVariable()
+    {
         return $this->variable;
     }
-    
-    public function setVariable( $variable ) {
+
+    public function setVariable($variable)
+    {
         $this->variable = $variable;
     }
-    
-    public function isDie(){
+
+    public function isDie()
+    {
         return $this->die;
     }
-    
-    public function setDie( $die ) {
-        if( is_bool( $die ) ) {
-            $this->die = $die;    
-        }
-        else {
-            $this->die = ( $die == "true" ) ? true : false;
+
+    public function setDie($die)
+    {
+        if (is_bool($die)) {
+            $this->die = (boolean) $die;
+        } else {
+            $this->die = ($die == "true") ? true : false;
         }
     }
-    
+
     /**
      * Set verb data
      * 
      * @param array $data
+     * @throws MyFusesVerbException
      */
-    public function setData( $data ) {
-        
-        parent::setData( $data );
-        
-        if( isset( $data[ "attributes" ][ "variable" ] ) ) {
-            $this->setVariable( $data[ "attributes" ][ "variable" ] );
-        }
-        else  {
+    public function setData($data)
+    {
+        parent::setData($data);
+
+        if (isset($data['attributes']['variable'])) {
+            $this->setVariable($data['attributes']['variable']);
+        } else  {
             $params = $this->getErrorParams();
             $params[ 'attrName' ] = "variable";
-            throw new MyFusesVerbException( $params, 
-                MyFusesVerbException::MISSING_REQUIRED_ATTRIBUTE );
+            throw new MyFusesVerbException($params,
+                MyFusesVerbException::MISSING_REQUIRED_ATTRIBUTE);
         }
-        
-        if( isset( $data[ "attributes" ][ "die" ] ) ) {
-            $this->setDie( $data[ "attributes" ][ "die" ] );
+
+        if (isset($data['attributes']['die'])) {
+            $this->setDie($data['attributes']['die']);
         }
-        
     }
-    
+
     /**
      * Return the parsed code
      *
      * @return string
      */
-    public function getParsedCode( $commented, $identLevel ) {
-        $strOut = parent::getParsedCode( $commented, $identLevel );
-        $strOut .= str_repeat( "\t", $identLevel );
-        $strOut .= "var_dump( " . $this->getVariable() . " );\n";
-        
-        if( $this->isDie() ) {
-            $strOut .= str_repeat( "\t", $identLevel );
+    public function getParsedCode($commented, $identLevel)
+    {
+        $strOut = parent::getParsedCode($commented, $identLevel);
+        $strOut .= str_repeat("\t", $identLevel);
+        $strOut .= "var_dump(" . $this->getVariable() . ");\n";
+
+        if($this->isDie()) {
+            $strOut .= str_repeat("\t", $identLevel);
             $strOut .= "die();\n";
         }
-        
         return $strOut;
     }
 }
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
