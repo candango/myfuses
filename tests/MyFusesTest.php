3b84558078c197418cfe757efde0d19b0806d45a
Flavio Garcia <piraz@candango.org>
2017-10-31 23:04:42 -0400

    Cleaning after changing/creating $_SESSION indexes.
    Refs: #1

--- tests/MyFusesTest.php
+++ tests/MyFusesTest.php
@@ -13,56 +13,98 @@ final class MyFusesTest extends TestCase
 
     public function testRootUrlOnRootAndHttp()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "http";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/index.php";
+
         $expectedUrl = "http://localhost/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testRootUrlOnRootAndHttps()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "https";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/index.php";
+
         $expectedUrl = "https://localhost/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testRootUrlOnDirectoryAndHttp()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "http";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/directory/index.php";
+
         $expectedUrl = "http://localhost/directory/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testRootUrlOnDirectoryAndHttps()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "https";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/directory/index.php";
+
         $expectedUrl = "https://localhost/directory/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testRootUrlOnMultiDirectoryAndHttp()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "http";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/directory/directory1/directory2/index.php";
+
         $expectedUrl = "http://localhost/directory/directory1/directory2/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testRootUrlOnMultiDirectoryAndHttps()
     {
+        $previousScriptName = $_SERVER['SCRIPT_NAME'];
+
         $_SERVER['REQUEST_SCHEME'] = "https";
         $_SERVER['HTTP_HOST'] = "localhost";
         $_SERVER['SCRIPT_NAME'] = "/directory/directory1/directory2/index.php";
+
         $expectedUrl = "https://localhost/directory/directory1/directory2/";
         $this->assertEquals($expectedUrl, MyFuses::getRootUrl());
+
+        unset($_SERVER['REQUEST_SCHEME']);
+        unset($_SERVER['HTTP_HOST']);
+        $_SERVER['SCRIPT_NAME'] = $previousScriptName;
     }
 
     public function testProtocolRequestSchemeHttp()
@@ -70,6 +112,7 @@ final class MyFusesTest extends TestCase
         $expectedProtocol = "http";
         $_SERVER['REQUEST_SCHEME'] = "http";
         $this->assertEquals($expectedProtocol, MyFuses::getProcotol());
+        unset($_SERVER['REQUEST_SCHEME']);
     }
 
     public function testProtocolRequestSchemeHttps()
@@ -77,6 +120,7 @@ final class MyFusesTest extends TestCase
         $expectedProtocol = "https";
         $_SERVER['REQUEST_SCHEME'] = "https";
         $this->assertEquals($expectedProtocol, MyFuses::getProcotol());
+        unset($_SERVER['REQUEST_SCHEME']);
     }
 
     public function testProtocolNoRequestScheme()
